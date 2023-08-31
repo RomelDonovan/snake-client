1dd9fe3a9baf957c418d51ae85869d3e4c6af956
RomelDonovan <RomelDonovan@live.ca>
2023-08-31 14:01:43 +0000

    Add stdin setup

--- play.js
+++ play.js
@@ -1,1 +1,21 @@
 const connect = require("./client");
+
+// setup interface to handle user input from stdin
+
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf-8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = (input) => {
+  if (input === '\u0003') {
+    console.log("Game closed");
+    process.exit();
+  }
+}
+
+setupInput();
